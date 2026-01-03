ffe45cc815a848afcb0c237b0c0e76b676e8c935
cougar15 <abhijay.banerjee11@gmail.com>
2026-01-03 16:53:32 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commonA/outtakeA/OuttakeA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commonA/outtakeA/OuttakeA.java
@@ -122,6 +122,10 @@ public class OuttakeA extends SubsystemBase{
                 m_kicker.setPosition(KICKER_RETRACTED_POS);
                 m_stop.setPosition(1);
                 break;
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             case SPIN_UP:
                 runFlywheelPID();
 
